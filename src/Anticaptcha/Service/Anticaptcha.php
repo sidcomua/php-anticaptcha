5725375568242c0b97a2dfac48e38108ad6654a7
reilag <reilag69@gmail.com>
2016-08-18 18:02:27 +0300

    Add new Ancticaptcha service

---
+++ src/Anticaptcha/Service/Anticaptcha.php
@@ -0,0 +1,16 @@
+<?php namespace Anticaptcha\Service;
+
+/**
+ * Class Anticaptcha
+ * @package Anticaptcha\Service
+ *
+ * Created by PhpStorm.
+ * User: Maksym Tymchyk
+ * Email: m.tymchyk@p1k.co.uk
+ * Date: 18.08.16
+ * Time: 17:45
+ */
+class Anticaptcha extends AbstractService
+{
+    protected $api_url = 'http://anti-captcha.com';
+}
